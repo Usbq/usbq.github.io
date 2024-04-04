1903c5e7b69338db4a28f65dccf9c600f93e574d
AshimeeAlt <161080149+AshimeeAlt@users.noreply.github.com>
2024-04-04 14:35:59 -0400

    Update sb-file-uploader-hoc.jsx

--- src/lib/sb-file-uploader-hoc.jsx
+++ src/lib/sb-file-uploader-hoc.jsx
@@ -104,7 +104,7 @@ const SBFileUploaderHOC = function (WrappedComponent) {
             } else {
                 // create <input> element and add it to DOM
                 this.inputElement = document.createElement('input');
-                this.inputElement.accept = '.sb,.sb2,.sb3';
+                this.inputElement.accept = '.uqb1,.sb,.sb2,.sb3,.zip';
                 this.inputElement.style = 'display: none;';
                 this.inputElement.type = 'file';
                 this.inputElement.onchange = this.handleChange; // connects to step 3
@@ -176,9 +176,9 @@ const SBFileUploaderHOC = function (WrappedComponent) {
         // used in step 6 below
         getProjectTitleFromFilename (fileInputFilename) {
             if (!fileInputFilename) return '';
-            // only parse title with valid scratch project extensions
-            // (.sb, .sb2, and .sb3)
-            const matches = fileInputFilename.match(/^(.*)\.sb[23]?$/);
+            // only parse title with valid scratch / usbq project extensions
+            // (.sb, .sb2, and .sb3) or (.uqb1)
+            const matches = fileInputFilename.match(/^(.*)\.sb[23]?$|^(.*)\.uqb1$/);
             if (!matches) return '';
             return matches[1].substring(0, 100); // truncate project title to max 100 chars
         }
