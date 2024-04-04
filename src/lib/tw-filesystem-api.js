c6b0ad27c32ca09a35015d02ddbbef6937876705
AshimeeAlt <161080149+AshimeeAlt@users.noreply.github.com>
2024-04-04 14:33:29 -0400

    Update tw-filesystem-api.js

--- src/lib/tw-filesystem-api.js
+++ src/lib/tw-filesystem-api.js
@@ -4,6 +4,12 @@ const showSaveFilePicker = fileName => window.showSaveFilePicker({
     suggestedName: fileName,
     types: [
         {
+            description: 'Usbq Project',
+            accept: {
+                'application/x.usbq.uqb1': '.uqb1'
+            }
+        },
+        {
             description: 'Scratch 3 Project',
             accept: {
                 'application/x.scratch.sb3': '.sb3'
@@ -18,9 +24,21 @@ const showOpenFilePicker = async () => {
         multiple: false,
         types: [
             {
+                description: 'Usbq Project',
+                accept: {
+                    'application/x.usbq.uqb1': ['.uqb1']
+                }
+            },
+            {
                 description: 'Scratch Project',
                 accept: {
                     'application/x.scratch.sb3': ['.sb', '.sb2', '.sb3']
+                }
+            },
+            {
+                description: 'ZIP File',
+                accept: {
+                    'application/zip': ['.zip']
                 }
             }
         ]
