e567a2e96d445cecc3a7e0bf498eae4c9bfd520e
Ashime Sho <a_2oo+gituseremail@outlook.com>
2024-09-22 22:01:04 -0400

    testing fr

--- scripts/updatedeps.mjs
+++ scripts/updatedeps.mjs
@@ -1,6 +1,10 @@
 import childProcess from 'child-process-es6-promise';
+import path from 'path';
 import fetch from 'node-fetch';
 import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 function Promise_all(values) {
   return new Promise((resolve, reject) => { 
@@ -18,30 +22,45 @@ function Promise_all(values) {
   }); 
 }
 
-await new Promise((resolve, reject) => {
-  fs.readFile('package-lock.json', 'utf8').then(async (lock) => {
-    try {
-      lock = JSON.parse(lock.toString());
-    } catch {
-      throw 'package-lock.json is not valid json?';
-    }
+await new Promise(async (resolve, reject) => {
+  const lock = await new Promise((resolve, reject) => {
+    fs.readFile('package-lock.json', 'utf8').then(lock => {
+      try {
+        lock = JSON.parse(lock.toString());
+      } catch {
+        throw 'package-lock.json is not valid json?';
+      }
+      resolve(lock);
+    }).catch(err => reject(err));
+  });
+  return await (async () => {
     const getSHA = name => {
       const sha = lock['packages'][`node_modules/${name}`]?.resolved;
       if (!sha) throw `Invalid resolved ${name}`;
+      console.log(`SHA1 of ${name}: ${sha.slice(sha.indexOf('#') + 1)}`);
       return sha.slice(sha.indexOf('#') + 1);
     };
     const updateDep = async (name, branch) => {
       branch = branch || 'develop';
       console.log(`Trying to update ${name}#${branch}`);
       const sha = getSHA(name);
-      let res = await fetch(`https://api.github.com/repos/Unsandboxed/${name}/commits/develop?limit=1`);     
+      let res = await fetch(`https://api.github.com/repos/Unsandboxed/${name}/commits/develop?limit=1&cache=${Date.now()}`);
       if (!res.ok) return `Failed to fetch commits for ${name}`;
       res = await res.json();
       if (!res || !res.sha) return 'Invalid SHA1 commit hash';
+      console.log(`${name} current sha: ${res.sha}`);
       if (sha === res.sha) return true;
       console.log(`Updating ${name}`);
       await new Promise((resolve, reject) => {
-        childProcess.spawn('npm', ['i', name, `github:Unsandboxed/${name}#${branch}`]).then(value => resolve(true)).catch(err => reject(err));
+        const cmd = ['npm', ['install', name, `github:Unsandboxed/${name}#${branch}`, '--package-lock-only']];
+        console.log('Running', cmd);
+        childProcess.spawn(...cmd, {
+          cwd: path.join(__dirname, '../')
+        }).then(
+          value => resolve(true)
+        ).catch(
+          err => reject(err)
+        );
       });
       console.log(`Updated ${name}`);
       return true;
@@ -55,7 +74,7 @@ await new Promise((resolve, reject) => {
     res = true;
     console.log('Updated dependancies\nRun:\n1. git add .\n2. git commit -m "Update dependancies"\n3. git push');
     resolve();
-  }).catch(err => {
+  })().catch(err => {
     console.error('Failed with error: ');
     console.error(err);
     resolve();
